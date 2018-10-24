d57885d0f9881383df170e219792f253062d8203
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2018-10-24 14:38:31 -0700

    added more notes

---
+++ basic python notes.py
@@ -0,0 +1,48 @@
+print("Hello World")
+
+# Apparently I'm going too slow, so I will speed up
+# This is a comment
+# This has no affect on the code
+# This is used for a variety of things, such as
+# 1. Making personal notes about my code
+# 2. Commenting out code that does not work
+
+print("Notice what is happening here ")
+print()  # This creates a new line
+print()  # Do you notice the underline here?
+# Hover over top it and see what is wrong
+
+# Watch
+print(5+3)
+print(5-3)
+print(5*3)
+print(5/3)
+print()
+
+# Semi-advanced math
+print("Figure this out...")
+print(6//4)
+print(12//3)
+print(9//4)
+print()  # This will ONLY give me a whole number
+
+print("Figure this out too...")
+print(6 % 4)
+print(5 % 3)
+print(9 % 4)
+
+# Defining Variables
+car_name = "Wiebe mobile"
+car_type = "Tesla"
+car_cylinders = 16
+car_mile_per_gallon = 0.01
+
+print("I have a car called %s. It's pretty nice")
+print("It has ")
+
+# Taking Input
+name = input("What is your name?")
+print("Hello %s " % name)
+
+age = input("How old are you?")
+print("")
