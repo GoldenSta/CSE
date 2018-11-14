8f73fdf4735aa88d104fabefa44287a4e1abd1b2
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2018-11-14 14:35:22 -0800

    added more codes

--- Seanti K. Lucky 7s.py
+++ Seanti K. Lucky 7s.py
@@ -1,27 +1,28 @@
 import random
-money = 15
+current_money = 15
 playing = True
 total_roles = 0
-Top_money = 15
-Top_roles = 0
+top_money = 15
+top_roles = 0
 
-while money > 0 and playing:
+while current_money > 0 and playing:
     dice_roll1 = random.randint(1, 6)
     dice_roll12 = random.randint(1, 6)
     print(dice_roll1, dice_roll12)
     role = dice_roll1 + dice_roll12
     if role == 7:
-        money += 4
+        current_money += 4
         print("Rolled a 7.")
         total_roles += 1
-        Top_money += 4
-        Top_roles += 1
-        if money > Top_money:
-            money = Top_money
+        top_money += 4
+        top_roles += 1
+        if current_money > top_money:
+            current_money = top_money
+            total_roles = top_roles
     else:
-        money -= 1
+        current_money -= 1
         print("Roll Again.")
         total_roles += 1
 
-print("You took %d rounds" % total_roles)
-print("You have %r money" % Top_money)
+print("Game Over. You took %d rounds" % total_roles)
+print("You lose %r dollars" % top_money)
