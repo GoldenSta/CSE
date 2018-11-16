3b07b85e34fbb7bd5ce67319afd3cb89030f2bd1
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2018-11-16 14:35:52 -0800

    added more codes

--- Seanti Keovilay-HangMan.py
+++ Seanti Keovilay-HangMan.py
@@ -1,1 +1,5 @@
 import random
+import string
+print("Let's play Hangman.")
+word = ['jello', 'tree', 'daydream', "midnight", "apple", "star", "sun", "moon", "winter", "summer",
+        "spring", "fall"]
