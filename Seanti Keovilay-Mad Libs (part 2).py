baf9ab807d3222147b0ff1b99fa2c32c611c1a41
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2018-11-28 14:36:55 -0800

    added more codes

---
+++ Seanti Keovilay-Mad Libs (part 2).py
@@ -0,0 +1,25 @@
+words = [
+    input("adjective?"),
+    input("noun?"),
+    input("adjective?"),
+    input("noun?"),
+    input("verb?"),
+    input("noun?"),
+    input("verb?"),
+    input("noun?"),
+    input("adjective?"),
+    input("verb?")
+]
+word_type = ["adjective?", "noun?", "adjective?", "noun?", "verb?", "noun?",
+             "verb?", "noun?", "adjective?", "verb?"]
+
+print("It was another %d " % words[0])
+print("day, in the city of %s" % words[1])
+print("Well if you can call it a %s")
+print("day. Today is %s")  # A holiday
+print("Everyone is %d ")
+print("around trying to finish their jobs to go %s")
+print(".Kids were %s")
+print("around the %s")
+print(", acting really %d")
+print("all the time but no one is %d")
