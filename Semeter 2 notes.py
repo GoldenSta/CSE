4b7973df539a422432d067f915efb24b2eff9b37
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-08 14:38:30 -0800

    add more codes

---
+++ Semeter 2 notes.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+#  hi
+
+cars = 5
+driving = True
+
+print("I have %s cars" % cars)
+
+age = input("How old are you?")
+print("%s? Really?" % age)
+
+colors = {"red", "blue", "yellow", "maroon", "grey"}
+colors.append("green")
+
+print(colors)
