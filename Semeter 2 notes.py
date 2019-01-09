1434ea492a48121f6f77aed311c8b21b0314f5ee
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-09 14:08:41 -0800

    added more notes

--- Semeter 2 notes.py
+++ Semeter 2 notes.py
@@ -9,7 +9,9 @@ print("I have %s cars" % cars)
 age = input("How old are you?")
 print("%s? Really?" % age)
 
-colors = {"red", "blue", "yellow", "maroon", "grey"}
+colors = ["red", "blue", "yellow", "maroon", "grey"]
 colors.append("green")
-
+colors.pop(0)
 print(colors)
+print(colors[1])
+print(len(colors))
