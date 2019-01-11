ea3c92cafa738815f108d1f78c02bbea964c2cc2
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-11 14:49:48 -0800

    added more codes

--- Seanti Keovilay-HangMan.py
+++ Seanti Keovilay-HangMan.py
@@ -1,4 +1,4 @@
 print("Let's play Hangman.")
 word = ['clock', 'sleep', 'daydream', "midnight", "nightmare", "star",
         "sun", "moon", "Winter", "Summer", "Spring", "Fall"]
-guesses = 5
+guesses = 8
