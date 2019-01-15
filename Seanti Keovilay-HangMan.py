046b0e97912a7466f93d5ac2b8d3cc721749eb6d
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-15 14:37:00 -0800

    added more codes

--- Seanti Keovilay-HangMan.py
+++ Seanti Keovilay-HangMan.py
@@ -1,9 +1,12 @@
+import string
 import random
 print("Let's play Hangman.")
 word = ['clock', 'sleep', 'daydream', "midnight", "nightmare", "star",
-        "sun", "moon", "Winter", "Summer", "Spring", "Fall"]
+        "sun", "moon", "Winter", "Summer", "Spring", "Fall", "cherry bomb"]
 letter_guesses = 0
 guesses_made = {}
 letter_guessed_right = {}
+print(list(string.ascii_letters))
 playing = True
 
+while guesses_made
--- Semeter 2 notes.py
+++ Semeter 2 notes.py
@@ -15,3 +15,9 @@ colors.pop(0)
 print(colors)
 print(colors[1])
 print(len(colors))
+
+import string
+print(list(string.ascii_letters))
+print(string.digits)
+print(string.punctuation)
+# print(string.printable)
