aab9237b4ddf99332d854c75f44817b786a0365a
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-16 14:36:13 -0800

    hangman

--- Seanti Keovilay-HangMan.py
+++ Seanti Keovilay-HangMan.py
@@ -1,12 +1,13 @@
 import string
 import random
 print("Let's play Hangman.")
-word = ['clock', 'sleep', 'daydream', "midnight", "nightmare", "star",
+word = ['cloud', 'sleep', 'daydream', "midnight", "nightmare", "star",
         "sun", "moon", "Winter", "Summer", "Spring", "Fall", "cherry bomb"]
 letter_guesses = 0
 guesses_made = {}
 letter_guessed_right = {}
-print(list(string.ascii_letters))
+random_word = random.choice(word)
 playing = True
 
-while guesses_made
+while guesses_made < 0 and playing:
+        letter = input("Guess a letter")
