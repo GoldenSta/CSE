0459ee110c453de61febe047ad0f353e5764083a
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-28 14:36:27 -0800

    hangman

--- Seanti Keovilay-HangMan.py
+++ Seanti Keovilay-HangMan.py
@@ -1,8 +1,8 @@
 import string
 import random
 print("Let's play Hangman.")
-word = ['cloud', 'sleep', 'daydream', "midnight", "nightmare", "star",
-        "sun", "moon", "Winter", "Summer", "Spring", "Fall", "cherry bomb"]
+word = ['cloud', 'sleep', 'daydream', "midnight", "nightmare", "star", "sun",
+        "moon", "Winter", "Summer", "Spring", "Fall", "cherry bomb", "clock"]
 letter_guesses = []
 guesses_made = 8
 random_word = random.choice(word)
@@ -10,20 +10,18 @@ playing = True
 hidden = list(random_word)
 
 for i in range(len(hidden)):
-    if hidden[i] == string.ascii_letters:
+    if hidden[i] in string.ascii_letters:
         hidden.pop(i)
         hidden.insert(i, "*")
 
+print(hidden)
 while guesses_made > 0 and playing:
     letter = input("Guess a letter")
-    if letter in random_word and letter not in letter_guesses:
-        print("you guess right")
-        guesses_made += 1
-        print("Guesses left: %d" % guesses_made)
-        if list(random_word) == hidden:
-            playing = False
     if letter in letter_guesses:
         print("You guessed this already.")
+    if letter in random_word and letter not in letter_guesses:
+        print("you guess right")
+        print("Guesses left: %d" % guesses_made)
     elif letter not in letter_guesses:
         print("Wrong letter")
         guesses_made -= 1
