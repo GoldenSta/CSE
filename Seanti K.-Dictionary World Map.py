42cb0ca6d1250c2e57e93fcd3427c12936f39d11
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-01-31 14:37:09 -0800

    notes

--- Seanti K.-Dictionary World Map.py
+++ Seanti K.-Dictionary World Map.py
@@ -4,7 +4,6 @@ world_map = {
         'DESCRIPTION': "This is the room that you are in.",
         'PATHS': {
             'NORTH': "PARKING_LOT"
-
         }
     },
     'PARKING_LOT': {
@@ -12,7 +11,26 @@ world_map = {
         'DESCRIPTION': "There are a few cars parked here.",
         'PATHS': {
             'SOUTH': "R19A"
-
         }
     }
 }
+
+# other variables
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN"]
+current_node = world_map["Rl9A"]  # This is your current location
+playing = True
+
+# controller
+while playing:
+    print(current_node["NAME"])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("I can't go that way.")
+    else:
+        print("Command not recognized.")
