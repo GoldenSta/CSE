5ad868b8bd0fd0fdeda8d5dcf62efc40375bb5ac
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-02-15 14:37:06 -0800

    object

--- Seanti K.-Object.py
+++ Seanti K.-Object.py
@@ -1,12 +1,15 @@
 class Camera(object):
-    def __init__(self, screen_resolution, pictures_left=100, color="Rose Gold"):
+    def __init__(self, screen_resolution, space_left=100, color="Rose Gold"):
         self.screen_resolution = screen_resolution
         self.color = color
         self.battery_left = 100
-        self.space_left = pictures_left
-        self.os = "Samung"
+        self.pictures_left = space_left
+        self.flashes = 10
+        self.brightness = 100
         self.functioning = True
 
-    def brightness(self, charge):
+    def pictures(self, time):
         if self.functioning:
-                
+            if self.pictures_left <= 100:
+                print("There is space left.")
+                self.pictures_left -= time
