7f80dd93cabc8abe95e91e364d203e32ecc64046
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-03-05 14:37:08 -0800

    item

--- Seanti Keovilay-Items.py
+++ Seanti Keovilay-Items.py
@@ -3,5 +3,23 @@ class Item(object):
         self.name = name
 
 
-class Marble(Item):
+class Flashlight(Item):
+    def __init__(self, name):
+        super(Item, self).__init__(name)
+        self.battery = 100
+        self.switch = False
+
+    def switch_on(self):
+        self.switch = True
+        print("You turn the flashlight on.")
+
+    def battery_left(self):
+        self.battery -= 5
+        print("You only have %s left." % self.battery)
+
+    def switch_off(self):
+        self.switch = False
+        print("You turn the flashlight off.")
+
+class Key(object):
     def __init__(self):
