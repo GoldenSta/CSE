4545f25429fa080969245046e2ffedc34c9528cb
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-03-06 14:36:41 -0800

    something

--- Seanti Keovilay-Items.py
+++ Seanti Keovilay-Items.py
@@ -1,25 +1,10 @@
 class Item(object):
-    def __init__(self, name):
+    def __init__(self, name, weight):
         self.name = name
+        self.weight = weight
 
 
-class Flashlight(Item):
-    def __init__(self, name):
-        super(Item, self).__init__(name)
-        self.battery = 100
-        self.switch = False
-
-    def switch_on(self):
-        self.switch = True
-        print("You turn the flashlight on.")
-
-    def battery_left(self):
-        self.battery -= 5
-        print("You only have %s left." % self.battery)
-
-    def switch_off(self):
-        self.switch = False
-        print("You turn the flashlight off.")
-
-class Key(object):
+class Inventory(object):
     def __init__(self):
+        self.items = {}
+
