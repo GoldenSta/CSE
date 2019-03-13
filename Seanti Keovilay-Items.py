7b2412828c967464efbea49c18f415372825c2be
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-03-13 14:39:44 -0700

    item

--- Seanti Keovilay-Items.py
+++ Seanti Keovilay-Items.py
@@ -32,58 +32,98 @@ class Inventory(Item):
             item1.hook = item2
         if isinstance(item2, FishingPole) and isinstance(item1, Hook):
             item2.hook = item1
+        if isinstance(item1, Box) and isinstance(item2, Coin):
+            item1.box = item2
+        if isinstance(item2, Box) and isinstance(item1, Coin):
+            item2.box = item1
             print("You combine %s and %d together.")
 
 
 class Flashlight(Item):
-    def __init__(self):
-        super(Flashlight, self).__init__("Flashlight", 5, "The flashlight is old.")
+    def __init__(self, name):
+        super(Flashlight, self).__init__(name, 5, "The flashlight is old.")
         self.batteries = None
 
 
 class Battery(Item):
-    def __init__(self):
-        super(Battery, self).__init__("Battery", 2, "You found some batters")
+    def __init__(self, name):
+        super(Battery, self).__init__(name, 2, "You found some batters")
 
 
 class Keys(Item):
-    def __init__(self):
-        super(Keys, self).__init__("Keys", 1, "The keys are rusting a little.")
+    def __init__(self, name):
+        super(Keys, self).__init__(name, 1, "The keys are rusting a little.")
 
 
 class Pills(Item):
-    def __init__(self):
-        super(Pills, self).__init__("Pills", 6, "Sometimes you get sick so you take at least 3.")
+    def __init__(self, name):
+        super(Pills, self).__init__(name, 6, "Sometimes you get sick so you take at least 3.")
 
 
 class Phone(Item):
-    def __init__(self):
-        super(Phone, self).__init__("Phone", 8, "You listen to music in hope it calms you down")
+    def __init__(self, name):
+        super(Phone, self).__init__(name, 8, "You listen to music in hope it calms you down")
         self.energy = 100
 
 
 class Marble(Item):
-    def __init__(self):
-        super(Marble, self).__init__("Marble", 1, "Its a orange marble with a slightly darker, rad star in the middle.")
+    def __init__(self, name):
+        super(Marble, self).__init__(name, 1, "Its a orange marble with a slightly darker, rad star in the middle.")
 
 
 class Camera(Item):
-    def __init__(self):
-        super(Camera, self).__init__("Camera", 7, "Taking pictures or videos with the camera is you past time.")
+    def __init__(self, name):
+        super(Camera, self).__init__(name, 7, "Taking pictures or videos with the camera is you past time.")
         self.energy = 100
 
 
 class Shovel(Item):
-    def __init__(self):
-        super(Shovel, self).__init__("Shovel", 15, "There are two spikes on either side of the handle.")
+    def __init__(self, name):
+        super(Shovel, self).__init__(name, 15, "There are two spikes on either side of the handle.")
 
 
 class FishingPole(Item):
-    def __init__(self):
-        super(FishingPole, self).__init__("Fishing Pole", 12, "The hook is missing somewhere.")
+    def __init__(self, name):
+        super(FishingPole, self).__init__(name, 12, "The hook is missing somewhere.")
         self.hook = None
 
 
 class Hook(Item):
-    def __init__(self):
-        super(Hook, self).__init__("Hook", 8, "It was stuck the cupboard in the kitchen.")
+    def __init__(self, name):
+        super(Hook, self).__init__(name, 8, "It was stuck the cupboard in the kitchen.")
+
+
+class Book(Item):
+    def __init__(self, name):
+        super(Book, self).__init__(name, 10, "The is called 'A Wrinkle in Time'. You loved the book")
+
+
+class StuffedAnimal(Item):
+    def __init__(self, name):
+        super(StuffedAnimal, self).__init__(name, 6, "Its a stuffed bunny. The fur seems old and dirty.")
+
+
+class Sweater(Item):
+    def __init__(self, name):
+        super(Sweater, self).__init__(name, 9, "The sweater is still intact.")
+
+
+class Coin(Item):
+    def __init__(self, name):
+        super(Coin, self).__init__(name, 4, "The coin is small. It looks like you could fit it in the box.")
+
+
+class Crystal(Item):
+    def __init__(self, name):
+        super(Crystal, self).__init__(name, 12, "Shine it in the light and it makes a rainbow")
+
+
+class Box(Item):
+    def __init__(self, name):
+        super(Box, self).__init__(name, 13, "The bos is dirty from digging it from the ground.")
+        self.coin = None
+
+
+class Character(object):
+    def __init__(self, name):
+        self.name = name
