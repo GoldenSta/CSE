325c9a5739d79a107756c284afefe361580b4f68
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-03-19 14:37:01 -0700

    adventure

--- Seanti Keovilay-Adventure Game.py
+++ Seanti Keovilay-Adventure Game.py
@@ -1,12 +1,12 @@
 class Room(object):
-    # This is a constructor
-    def __init__(self, name, description, north=None, south=None, west=None, east=None):
+    def __init__(self, name, description, north=None, south=None, west=None, east=None, item=None):
         self.name = name
         self.description = description
         self.north = north
         self.south = south
         self.west = west
         self.east = east
+        self.item = item
 
 
 class Dog(object):
@@ -39,7 +39,6 @@ class Player(object):
         """
         room_name = getattr(self.current_location, direction)
         return globals()[room_name]
-        # getarr(R19A, "north")
 
 
 class Item(object):
@@ -101,9 +100,9 @@ class Keys(Item):
         super(Keys, self).__init__(name, 1, "The keys are rusting a little.")
 
 
-class Pills(Item):
+class Medicine(Item):
     def __init__(self, name):
-        super(Pills, self).__init__(name, 6, "Sometimes you get sick so you take at least 3.")
+        super(Medicine, self).__init__(name, 6, "Sometimes you get sick so you take at least 3.")
 
 
 class Phone(Item):
@@ -179,26 +178,40 @@ class Character(object):
         self.weight = weight
 
 
+shovel = Shovel("Shovel")
 battery = Battery("Battery")
 flashlight = Flashlight("Flashlight")
 coin = Coin("Coin")
 box = Box("Box")
+keys = Keys("Keys")
+fishing_pole = FishingPole("FishingPole")
+phone = Phone("Phone")
+stuffed_animal = StuffedAnimal("StuffedAnimal")
+medicine = Medicine("Medicine")
+marble = Marble("Marble")
+camera = Camera("Camera")
+crystal = Crystal("Crystal")
+hook = Hook("Hook")
+book = Book("Book")
+sweater = Sweater("Sweater")
 player_bag = Inventory("Bag")
+
 Road = Room("Old Road", "The road that brought you here.", "Your Old House", "Your Car", None, None)
-Car = Room("Your Car", "You arrived in the car with a bag inside.", "Road", None, None, None)
+Car = Room("Your Car", "You arrived in the car with a bag inside.", "Road", None, None, None, "Keys")
 House = Room("Your Old House", "This is the house you grew up in your life.", "Front Door", "Road", None, None)
 Backyard = Room("The Backyard", "You always play here whenever something bad happen inside the house", None,
                 "Back Door", None, "Big Tree")
 West_Forest = Room("West Side Forest", "You can hear the birds chirping from the north. There is a playground straight"
                                        "ahead.", "Field", None, "Playground", "West Door")
 Big_Tree = Room("Big Tree", "The tree you planted back grew bigger than you thought it would.", None, None, "Backyard",
-                "Crystal Lake")
+                "Crystal Lake", "Box")
 Field = Room("Abandon Field", "The field had been abandoned for many years.", "Grassy Hill", "West Forest", None, None)
-Playground = Room("Old Playground", "It looks like it about to fall apart.", None, None, None, "West Forest")
+Playground = Room("Old Playground", "It looks like it about to fall apart.", None, None, None,
+                  "West Forest", "StuffedAnimal")
 East_Forest = Room("East Forest", "Keep going east, you can see the barn from here. North, there is a lake.",
                    "Crystal Lake", None, "East Door", "Barn")
 Crystal_Lake = Room("Crystal Lake", "You loved going fishing here with your friends or family.", None,
-                    "East Forest", "Big Tree", None)
+                    "East Forest", "Big Tree", None, "Crystal")
 Barn = Room("Old Barn", "It's filled with hay and nothing else.", "Mini Farm", "Tool Shed", "East Forest", None)
 Tool_Shed = Room("Tool Shed", "There is a fishing pole and a shovel inside.", "Barn", None, None, None)
 Mini_Farm = Room("Mini Farm", "You grew different types of plants here.", None, "Barn", None, None)
@@ -209,10 +222,11 @@ Living_Room = Room("Living Room", "There is nothing inside the living room expec
                    "West Door", "Hallway")
 West_Door = Room("West Door", "It leads to the West Forest.", None, None, "West Forest", "Living Room")
 Hallway = Room("Hallway", "There is a bedroom to the north and the bathroom in the south. There is a door leading"
-                          "to the East Forest", "Bedroom", "Bathroom", None, "East Door")
-Kitchen = Room("Moldy Kitchen", "The kitchen hasn't been clean for years.", "Back Door", "Living Room", None, None)
+                          "to the East Forest", "Bedroom", "Bathroom", None, "East Door", "Camera")
+Kitchen = Room("Moldy Kitchen", "The kitchen hasn't been clean for years.",
+               "Back Door", "Living Room", None, None, "Hook")
 Back_Door = Room("Back Door", "It leads to the backyard.", "Backyard", "Kitchen", None, None)
-Bedroom = Room("Your Bedroom", "Everything is still in the same place.", None, "Hallway", None, None)
+Bedroom = Room("Your Bedroom", "Everything is still in the same place.", None, "Hallway", None, None, "Sweater")
 Bathroom = Room("Broken Bathroom", "The place have been collected cobwebs.", "Hallway", None, None, None)
 East_Door = Room("East Door", "The door leads to the East Forest.", None, None, "Hallway", "East Forest")
 
@@ -221,7 +235,6 @@ player = Player(Road)
 directions = ['north', 'south', 'east', 'west', 'up', 'down']
 playing = True
 
-# controller
 while playing:
     print(player.current_location.name)
     print(player.current_location.description)
