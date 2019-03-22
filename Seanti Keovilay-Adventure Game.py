81b21d46a421a3b5e3074ed4b0b84e473269be31
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-03-22 14:46:14 -0700

    adventure

--- Seanti Keovilay-Adventure Game.py
+++ Seanti Keovilay-Adventure Game.py
@@ -178,6 +178,7 @@ class Character(object):
         self.weight = weight
 
 
+player_bag = Inventory("Bag")
 shovel = Shovel("Shovel")
 battery = Battery("Battery")
 flashlight = Flashlight("Flashlight")
@@ -192,9 +193,8 @@ marble = Marble("Marble")
 camera = Camera("Camera")
 crystal = Crystal("Crystal")
 hook = Hook("Hook")
-book = Book("Book")
+book = Book("A Wrinkle in Time")
 sweater = Sweater("Sweater")
-player_bag = Inventory("Bag")
 
 Road = Room("Old Road", "The road that brought you here.", "House", "Car", None, None, "Coin")
 Car = Room("Your Car", "You arrived in the car with a bag inside.", "Road", None, None, None, "Keys")
@@ -214,14 +214,14 @@ East_Forest = Room("East Forest", "Keep going east, you can see the barn from he
 Crystal_Lake = Room("Crystal Lake", "You loved going fishing here with your friends or family.", None,
                     "East_Forest", "Big_Tree", None, "Crystal")
 Barn = Room("Old Barn", "It's filled with hay and nothing else.", "Mini_Farm", "Tool_Shed", "East_Forest", None)
-Tool_Shed = Room("Tool Shed", "There is a fishing pole and a shovel inside.", "Barn", None, None, None)
-Mini_Farm = Room("Mini Farm", "You grew different types of plants here.", None, "Barn", None, None)
+Tool_Shed = Room("Tool Shed", "There is a fishing pole and a shovel inside.", "Barn", None, None, None, "Shovel")
+Mini_Farm = Room("Mini Farm", "You grew different types of plants here.", None, "Barn", None, None, "FishingPole")
 Grassy_Hill = Room("Grassy Hill", "You loved to cloud gaze here or star gaze.", None, "Field", "Cave", None)
 Cave = Room("Abandoned Cave", "You never went inside there as it was too dark to see.", None, None,
-            None, "Grassy_Hill")
+            None, "Grassy_Hill", "Marble")
 Front_Door = Room("Front Door", "It leads to the living room if open.", "Living_Room", "Road", None, None)
 Living_Room = Room("Living Room", "There is nothing inside the living room expect for the west door.", "Kitchen", None,
-                   "West_Door", "Hallway")
+                   "West_Door", "Hallway", "Book")
 West_Door = Room("West Door", "It leads to the West Forest.", None, None, "West_Forest", "Living_Room")
 Hallway = Room("Hallway", "There is a bedroom to the north and the bathroom in the south. There is a door leading"
                           "to the East Forest", "Bedroom", "Bathroom", "Living_Room", "East_Door", "Camera")
@@ -229,7 +229,7 @@ Kitchen = Room("Moldy Kitchen", "The kitchen hasn't been clean for years.",
                "Back_Door", "Living_Room", None, None, "Hook")
 Back_Door = Room("Back Door", "It leads to the backyard.", "Backyard", "Kitchen", None, None)
 Bedroom = Room("Your Bedroom", "Everything is still in the same place.", None, "Hallway", None, None, "Sweater")
-Bathroom = Room("Broken Bathroom", "The place have been collected cobwebs.", "Hallway", None, None, None)
+Bathroom = Room("Broken Bathroom", "The place have been collected cobwebs.", "Hallway", None, None, None, "Medicine")
 East_Door = Room("East Door", "The door leads to the East Forest.", None, None, "Hallway", "East_Forest")
 
 player = Player(Road)
