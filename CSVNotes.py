7b8badebd48d79fe61d3d7fc8a168262b494b950
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-04-24 14:37:15 -0700

    csv

---
+++ CSVNotes.py
@@ -0,0 +1,9 @@
+import csv
+
+with open("Book1.csv", 'r') as old_csv:
+    reader = csv.reader(old_csv)
+    for row in reader:
+        old_number = row[0]
+        print(int(old_number) + 1)
+        # print(int(old_number) + 1)
+        print(old_number)
