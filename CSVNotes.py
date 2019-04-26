c053bf972a8f5ce7ccdfd1489f1d695d8d3f2cba
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-04-26 14:39:13 -0700

    csv

--- CSVNotes.py
+++ CSVNotes.py
@@ -1,24 +1,65 @@
 import csv
 
+
+def validate(num: str):
+    if divisible_by_2(num) and divisible_by_three(num):
+        return True
+    return False
+
+
+def divisible_by_three(num: str):
+    first_num = int(num[0])  # this is the first number
+    if first_num % 3 == 0:
+        return True
+    return False
+
+
+def divisible_by_2(num: str):
+    first_num = int(num[0])  # this is the first number
+    if first_num % 2 == 0:
+        return True
+    return False
+
+
+def is_first_num_odd(num: str):
+    first_num = int(num[0])
+    if first_num % 2 == 1:
+        return True
+    return False
+
+def is_second_num_even(num: str):
+    
 # with open("Book1.csv", 'r') as old_csv:
 #     reader = csv.reader(old_csv)
 #     for row in reader:
 #         old_number = row[0]
 #         # print(int(old_number) + 1)
-#         print(old_number)
+#        print(old_number)
+#
+# with open("Book1.csv", 'r') as old_csv:
+#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+#         reader = csv.reader(old_csv)
+#         writer = csv.writer(new_csv)
+#         print("Processing...")
+#
+#         for row in reader:
+#             # print(int(old_number) + 1)
+#             old_number = row[0]
+#             first_num = int(old_number[0])
+#             # if first_num % 2 == 0:
+#             #     writer.writerow(row)
+#             if first_num == 4:
+#                 writer.writerow(row)
+#     print("Done")
+
 
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing File...")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
-        print("Processing...")
-
         for row in reader:
-            # print(int(old_number) + 1)
             old_number = row[0]
-            first_num = int(old_number[0])
-            # if first_num % 2 == 0:
-            #     writer.writerow(row)
-            if first_num == 4:
+            if validate(old_number):
                 writer.writerow(row)
     print("Done")
