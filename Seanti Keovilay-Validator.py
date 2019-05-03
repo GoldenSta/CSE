f8c7f3e4dbf9ebc72c91b7096aba0ac5438cc403
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-05-03 14:49:09 -0700

    csv

--- Seanti Keovilay-Validator.py
+++ Seanti Keovilay-Validator.py
@@ -1,4 +1,4 @@
-test_num = "6076470818000080"
+import csv
 
 
 def reverse_it(list):
@@ -6,18 +6,30 @@ def reverse_it(list):
 
 
 def validate(num: str):
-    num_list = list(test_num)
+    num_list = list(num)
     last_num = int(num_list[len(num_list) - 1])
+    num_list.pop(15)
     new_list = reverse_it(num_list)
-    new_list.remove(last_num)
-    for index in range(len(num)):
-        num_list[index] = int(num_list[index])
+    for index in range(len(num_list)):
+        new_list[index] = int(new_list[index])
         if index % 2 == 0:
             new_list[index] *= 2
             if new_list[index] > 9:
                 new_list[index] -= 9
+    all_num = sum(new_list)
+    if all_num % 10 == last_num:
+        return True
+    else:
+        return False
 
-    print(new_list)
 
-
-print(validate(test_num))
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing File...")
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            old_number = row[0]
+            if validate(old_number):
+                writer.writerow(row)
+    print("Done")
