b51e9383d3d45f28cd31350d2e0ca44bb1f2ddd3
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-05-10 14:48:28 -0700

    csv

--- Seanti Keovilay-Sale Records.py
+++ Seanti Keovilay-Sale Records.py
@@ -2,9 +2,11 @@ import csv
 
 with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
+    total = 0
     for row in reader:
         item = row[2]
         profit = row[13]
-        if "fruits" == item:
-            profit_list = sum(profit)
-        print(profit_list)
+        if "Fruits" == item:
+            total += float(profit)
+
+print(total)
