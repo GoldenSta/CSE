1c3f0b0ca9b9362f82fd8b217b2834da9867df91
DayDreamer <42391948+GoldenSta@users.noreply.github.com>
2019-05-15 14:39:03 -0700

    csv

--- Seanti Keovilay-Sale Records.py
+++ Seanti Keovilay-Sale Records.py
@@ -4,7 +4,8 @@ profit_dict = {}
 top_profit = 0
 region_dict = {}
 region_profit = 0
-
+unit_dict = {}
+unit_profit = 0
 
 with open("Sales Records.csv", "r") as old_csv:
     reader = csv.reader(old_csv)
@@ -20,9 +21,11 @@ with open("Sales Records.csv", "r") as old_csv:
         try:
             profit_dict[item] += float(profit)
             region_dict[region] += float(profit)
+            unit_dict[unit] += float(profit)
         except KeyError:
             profit_dict[item] = float(profit)
             region_dict[region] = float(profit)
+            unit_dict[unit] = float(profit)
 
         if profit_dict[item] > top_profit:
             top_profit = profit_dict[item]
@@ -30,12 +33,19 @@ with open("Sales Records.csv", "r") as old_csv:
         if region_dict[region] > region_profit:
             region_profit = region_dict[region]
 
+        if unit_dict[unit] > unit_profit:
+            unit_profit = unit_dict[unit]
+
     for key, value in profit_dict.items():
         if value == top_profit:
             print(key)
 
     for key, value in region_dict.items():
         if value == region_profit:
+            print(key)
+
+    for key, value in unit_dict.items():
+        if value == unit_profit:
             print(key)
 
 print(total)
