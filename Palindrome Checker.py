bc409f27d185fac0f31290aec4c9ffed977bb70a
DayDreamer <cdyq@wetigers.org>
2019-09-06 19:35:45 -0700

    checker

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,10 +1,33 @@
 import string
+print(string.punctuation)
 
-string = input("Please a string: \n").lower()
-reverse_str = ''.join(reversed(string))
+String = input("Please add a string: \n").lower()
+reverse_str = ''
+String = String.replace(" ", "")
+reverse_str = reverse_str.replace(" ", "")
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+no_punct = ""
+str1 = ''
 
 
-if string == reverse_str[::-1]:
-    print("{} is a Palindrome String".format(string))
+for char in String:
+    if char not in punctuations:
+        no_punct = no_punct + char
+
+
+def isPalindrome(s):
+    rev = ''.join(reversed(s))
+    if s == rev:
+        return True
+    return False
+
+
+for i in String:
+    str1 = i + str1
+print("String in reverse Order :", str1)
+
+
+if String == str1:
+    print("%s is a Palindrome String" % no_punct)
 else:
-    print("{} isn't not a Palindrome String".format(string))
+    print("%s isn't a Palindrome String" % no_punct)
